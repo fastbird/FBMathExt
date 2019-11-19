72f5f8e4612ce83e4a7ba89cc4cb3a5f3c166759
jungwan <jungwan82@naver.com>
2019-11-20 06:22:15 +0900

    Shadow Matrix

--- MathExt.cpp
+++ MathExt.cpp
@@ -16,17 +16,17 @@ bool PlaneIsInfinite(const glm::vec4& plane)
 		!std::isfinite(plane.w);
 }
 
-//glm::vec4 PlaneNormalize(const glm::vec4& plane)
-//{
-//	float fLengthSq = glm::length(plane);
-//	// Prevent divide by zero
-//	if (fLengthSq > 0)
-//	{
-//		fLengthSq = 1.0f / fLengthSq;
-//	}
-//	glm::vec4 vResult = plane * fLengthSq;
-//	return vResult;
-//}
+glm::vec4 PlaneNormalize(const glm::vec4& plane)
+{
+	float fLengthSq = glm::length(glm::vec3(plane));
+	// Prevent divide by zero
+	if (fLengthSq > 0)
+	{
+		fLengthSq = 1.0f / fLengthSq;
+	}
+	glm::vec4 vResult = plane * fLengthSq;
+	return vResult;
+}
 
 glm::mat4 fb::MatrixReflect(const glm::vec4& reflectionPlane)
 {
@@ -35,7 +35,7 @@ glm::mat4 fb::MatrixReflect(const glm::vec4& reflectionPlane)
 
 	static const glm::vec4 NegativeTwo{ -2.0f, -2.0f, -2.0f, 0.0f };
 
-	glm::vec4 P = glm::normalize(reflectionPlane);
+	glm::vec4 P = PlaneNormalize(reflectionPlane);
 	glm::vec4 S = P * NegativeTwo;
 
 	glm::vec4 A(P.x, P.x, P.x, P.x);
@@ -50,4 +50,23 @@ glm::mat4 fb::MatrixReflect(const glm::vec4& reflectionPlane)
 	M[3] = D * S + glm::identity<glm::mat4>()[3];
 	M = glm::transpose(M);
 	return M;
+}
+
+glm::mat4 fb::MatrixShadow(const glm::vec4& shadowPlane, const glm::vec4& toLightDir)
+{
+	auto normalizedP = PlaneNormalize(shadowPlane);
+	float d = shadowPlane.w;
+	float nDotL = glm::dot(normalizedP, toLightDir);
+	normalizedP = -normalizedP;
+	auto row0 = glm::vec4(normalizedP.x, normalizedP.x, normalizedP.x, normalizedP.x) * toLightDir;
+	row0[0] += nDotL;
+	auto row1 = glm::vec4(normalizedP.y, normalizedP.y, normalizedP.y, normalizedP.y) * toLightDir;
+	row1[1] += nDotL + d * toLightDir.w;
+	auto row2 = glm::vec4(normalizedP.z, normalizedP.z, normalizedP.z, normalizedP.z) * toLightDir;
+	row2[2] += nDotL + d * toLightDir.w;
+	auto row3 = -glm::vec4(toLightDir) * d;
+	row3[3] = nDotL;
+
+	// rows go to columns
+	return glm::mat4(row0, row1, row2, row3);
 }
--- MathExt.h
+++ MathExt.h
@@ -4,4 +4,6 @@
 namespace fb
 {
 	glm::mat4 MatrixReflect(const glm::vec4& reflectionPlane);
+	// toLightDir.w == 0 for Directional light, otherwise its for PointLights.
+	glm::mat4 MatrixShadow(const glm::vec4& shadowPlane, const glm::vec4& toLightDir);
 }
